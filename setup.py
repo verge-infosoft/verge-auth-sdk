f562a8289a9fa7258a1384782013493c6e161949
abhinav jha <abhinav_jha@hotmail.com>
2025-12-03 22:43:52 +0530

    publishing to pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extensions = [
 
 setup(
     name="verge_auth_sdk",
-    version="0.1.0",
+    version="0.1.4",
     packages=find_packages(),
     ext_modules=cythonize(extensions, compiler_directives={
                           "language_level": "3"}),
